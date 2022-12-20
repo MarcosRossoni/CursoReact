4830309587e779e4455080c2d2afc92228c15b51
marcosrossoni <marcosrossoni9@gmail.com>
2022-12-20 00:05:44 -0300

    conclusao de menus e inicio de cadastros

--- CursoSpringBootAngularFrontend/src/app/app.component.ts
+++ CursoSpringBootAngularFrontend/src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component} from '@angular/core';
+// @ts-ignore
+import jQuery from 'jquery'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
   title = 'CursoSpringBootAngularFrontend';
+
+  ngAfterViewInit() {
+    (function($) {
+      "use strict";
+      var path = window.location.href; // because the 'href' property of the DOM element is the absolute path
+      $("#layoutSidenav_nav .sb-sidenav a.nav-link").each(function() {
+        // @ts-ignore
+        if (path === this.href) {
+          // @ts-ignore
+          $(this).addClass("active");
+        } else {
+          return;
+        }
+      });
+
+      // @ts-ignore
+      $("#sidebarToggle").on("click", function (e) {
+        e.preventDefault();
+        $("body").toggleClass("sb-sidenav-toggled");
+      });
+    })(jQuery);
+  }
 }
