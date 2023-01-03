f3490b50b1ac320dfcf3e01ac17f6a2002f13ec8
marcosrossoni <marcosrossoni9@gmail.com>
2023-01-02 22:11:59 -0300

    tratamento de croos origin

--- CursoSpringBootAngularBackend/src/main/java/br/com/cursospringbootangularbackend/model/entity/Cliente.java
+++ CursoSpringBootAngularBackend/src/main/java/br/com/cursospringbootangularbackend/model/entity/Cliente.java
@@ -1,10 +1,12 @@
 package br.com.cursospringbootangularbackend.model.entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.NonNull;
 import org.hibernate.validator.constraints.br.CPF;
 import lombok.Data;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @Entity
@@ -16,10 +18,12 @@ public class Cliente {
     private Integer id;
 
     @Column(nullable = false, length = 150)
+    @NotNull(message = "campo.nome.obrigatorio")
     private String nome;
 
-    @CPF
+    @CPF(message = "campo.cpf.invalido")
     @Column(nullable = false, length = 11)
+    @NotNull(message = "campo.cpf.obrigatorio")
     private String cpf;
 
     @Column(updatable = false)
