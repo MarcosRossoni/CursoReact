dc2372bdbf8deebe0ee98aa48c3b5525206df3fa
marcosrossoni <marcosrossoni9@gmail.com>
2023-06-21 13:28:49 -0300

    implementacao de styles

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,15 +1,20 @@
 import './App.css';
 import SayMyName from "./components/SayMyName";
 import Pessoa from "./components/Pessoa";
+import List from "./components/List";
+import Frase from "./components/Frase";
 
 function App() {
     const nome = 'Mariah'
     return (
         <div className="App">
+            <Frase/>
+            <Frase/>
             <SayMyName nome="Marcos"/>
             <SayMyName nome="Ana"/>
             <SayMyName nome={nome}/>
             <Pessoa nome="Marcos" idade="30" profissao="Programador" foto="https://via.placeholder.com/150"/>
+            <List/>
         </div>
     );
 }
